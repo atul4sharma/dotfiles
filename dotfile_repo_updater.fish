8de856ab4f1022f12e52b2b82d9571438cac0462
Atul Sharma <atulsharma406@gmail.com>
2020-10-17 20:00:29 +0530

    moved the functions to a main_fn

--- dotfile_repo_updater.fish
+++ dotfile_repo_updater.fish
@@ -19,20 +19,25 @@ function copy_from_to --description 'Copy files from $argv[1] to $argv[2]'
     cp -r $argv[1] $argv[2]
 end
 
-set -l file_list_to_check \
-    "$HOME/.vimrc,$DOTFILE_REPO/vim/vimrc" \
-    "$HOME/.config/alacritty/alacritty.yml,$DOTFILE_REPO/alacritty/alacritty.yml" \
-    "$HOME/.tmux.conf,$DOTFILE_REPO/tmux/tmux.conf" \
-    "$HOME/.gitconfig,$DOTFILE_REPO/git_configs/gitconfig" \
-    "$HOME/.gitignore_global,$DOTFILE_REPO/git_configs/gitignore_global" \
-    "$HOME/.config/fish/,$DOTFILE_REPO/fish_configs/"
+function main_fn
+    set -l file_list_to_check \
+        "$HOME/.vimrc,$DOTFILE_REPO/vim/vimrc" \
+        "$HOME/.config/alacritty/alacritty.yml,$DOTFILE_REPO/alacritty/alacritty.yml" \
+        "$HOME/.tmux.conf,$DOTFILE_REPO/tmux/tmux.conf" \
+        "$HOME/.gitconfig,$DOTFILE_REPO/git_configs/gitconfig" \
+        "$HOME/.gitignore_global,$DOTFILE_REPO/git_configs/gitignore_global" \
+        "$HOME/.config/fish/,$DOTFILE_REPO/fish_configs/"
 
-for item in $file_list_to_check
-    set __elements (string split ',' $item)
-    printf '********************************************************************************\n'
-    check_diff $__elements[1] $__elements[2]
-    set -l __diff_status $status
-    if test $__diff_status -ne 0
-        copy_from_to $__elements[1] $__elements[2]
+    for item in $file_list_to_check
+        set __elements (string split ',' $item)
+        printf '********************************************************************************\n'
+        check_diff $__elements[1] $__elements[2]
+        set -l __diff_status $status
+        if test $__diff_status -ne 0
+            copy_from_to $__elements[1] $__elements[2]
+        end
     end
 end
+
+main_fn
+
